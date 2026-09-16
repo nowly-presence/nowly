db4d04c4dead811390ce5408127eb63223b57cea
Gaëtan H <me@gaetanhus.fr>
2026-09-16 19:18:00 +0200

    refactor(web): remove legacy pages and obsolete code

--- apps/web/app/(presences)/library/[item]/loading.tsx
+++
@@ -1,8 +0,0 @@
-import { PresenceDetailSkeleton } from "@/components/library/presence-detail/presence-detail-skeleton";
-import type { FC } from "react";
-
-const Loading: FC = () => {
-  return <PresenceDetailSkeleton />;
-};
-
-export default Loading;
--- apps/web/app/ads.txt/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-
-export const GET = () => {
-  return new NextResponse("google.com, pub-6330177306711077, DIRECT, f08c47fec0942fa0\n", {
-    headers: { "Content-Type": "text/plain; charset=utf-8" },
-  });
-};
