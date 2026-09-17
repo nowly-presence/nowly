e90d2aaa4f50fb468fd3a04208cab625a31c5566
Gaëtan H <me@gaetanhus.fr>
2026-09-18 00:47:39 +0200

    refactor(web): remove supporter redeem flow

--- apps/web/lib/constants.ts
+++ apps/web/lib/constants.ts
@@ -4,8 +4,6 @@ export const DISCORD_INVITE_URL = "https://discord.gg/MnZap7czgB";
 export const DISCORD_SITE_URL = "https://discord.com";
 export const TWITTER_URL = "https://x.com/nowly";
 export const KOFI_URL = "https://ko-fi.com/nowly";
-export const GITHUB_SPONSORS_URL = "https://github.com/sponsors/nowly-presence";
-export const REDEEM_PAGE_URL = "https://nowly.me/redeem";
 
 export const CDN_INSTALLER_BASE_URL = "https://cdn.nowly.me/installer";
 export const DESKTOP_LATEST_MANIFEST_URL = `${CDN_INSTALLER_BASE_URL}/latest.json`;
