e58b8d20e31bd13072be497c39e286c5fae649bf
Gaëtan H <me@gaetanhus.fr>
2026-09-18 16:00:29 +0200

    feat(ui): add shared @nowly/ui component library

---
+++ packages/ui/src/button.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { Button as ButtonPrimitive } from "@base-ui/react/button";
+import { buttonVariants, type ButtonVariantProps } from "./button-variants";
+import { cn } from "./utils";
+
+function Button({
+  className,
+  variant = "default",
+  size = "default",
+  ...props
+}: ButtonPrimitive.Props & ButtonVariantProps) {
+  return (
+    <ButtonPrimitive
+      data-slot="button"
+      className={cn(buttonVariants({ variant, size }), className)}
+      {...props}
+    />
+  );
+}
+
+export { Button };
